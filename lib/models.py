c3441bb25b9196d5cbb25ee3fdb850b1e49b85e0
Natasha Onsongo <kemmienatasha@gmail.com>
2025-05-28 20:42:46 +0300

    Add SQLAlchemy models with Company, Dev, and Freebie classes and relationships

---
+++ lib/models.py
@@ -0,0 +1,45 @@
+from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
+
+convention = {
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+}
+metadata = MetaData(naming_convention=convention)
+
+Base = declarative_base(metadata=metadata)
+
+class Company(Base):
+    __tablename__ = 'companies'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    founding_year = Column(Integer())
+
+    def __repr__(self):
+        return f'<Company {self.name}>'
+
+class Dev(Base):
+    __tablename__ = 'devs'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+
+    def __repr__(self):
+        return f'<Dev {self.name}>'
+
+class Freebie(Base):
+    __tablename__ = 'freebies'
+
+    id = Column(Integer(), primary_key=True)
+    item_name = Column(String())
+    value = Column(Integer())
+
+    company_id = Column(Integer(), ForeignKey('companies.id'))
+    dev_id = Column(Integer(), ForeignKey('devs.id'))
+
+    company = relationship("Company", backref="freebies")
+    dev = relationship("Dev", backref="freebies")
+
+    def __repr__(self):
+        return f"<Freebie {self.item_name} worth ${self.value} from {self.company.name} to {self.dev.name}>"
